a6751e4fa4c1f4911c8e65c949a7cfe951850505
dsarab <100468034+dsarab@users.noreply.github.com>
2022-08-21 12:00:02 +0200

    Update main.tf
    modified main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
+# main.tf
 terraform {
-  backend "remote" {
-    organization = "hashicorp-learn"    
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "hashicorp-learn"
     workspaces {
       name = "learn-terraform-pipelines-k8s"
     }
   }
-required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.55"
-    }
 }
 
-required_version = ">= 1.1.0"
-}
 
 
 
